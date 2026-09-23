02a96f907b949a180c0fbbcc6e4d724092b27176
jahidrefat238-oss <jahidrefat238@gmail.com>
2026-09-23 18:16:41 +0600

    Update technology icons and badge data from url with updated json file

--- src/components/TechnologySection/TechnologyIcon.tsx
+++ src/components/TechnologySection/TechnologyIcon.tsx
@@ -1,17 +1,4 @@
 import type { ITechnology } from "../../types/technology";
-import { FaJava } from "react-icons/fa";
-import {
-  SiReact,
-  SiVuedotjs,
-  SiSvelte,
-  SiNextdotjs,
-  SiNodedotjs,
-  SiPostgresql,
-  SiJavascript,
-  SiTypescript,
-  SiTailwindcss,
-  SiDocker,
-} from "react-icons/si";
 
 interface TechnologyIconProps {
   technology: ITechnology;
@@ -19,45 +6,11 @@ interface TechnologyIconProps {
 
 const TechnologyIcon = ({ technology }: TechnologyIconProps) => {
   return (
-    <div className="text-3xl">
-      {technology.name === "React" && <SiReact className="text-[#61DAFB]" />}
-
-      {technology.name === "Vue.js" && (
-        <SiVuedotjs className="text-[#4FC08D]" />
-      )}
-
-      {technology.name === "Svelte" && <SiSvelte className="text-[#FF3E00]" />}
-
-      {technology.name === "Next.js" && <SiNextdotjs className="text-black" />}
-
-      {technology.name === "Node.js" && (
-        <SiNodedotjs className="text-[#339933]" />
-      )}
-
-      {technology.name === "PostgreSQL" && (
-        <SiPostgresql className="text-[#4169E1]" />
-      )}
-
-      {technology.name === "Redis" && (
-        <img src="/redis_logo.png" alt="Redis" className="h-12 w-15" />
-      )}
-
-      {technology.name === "JavaScript" && (
-        <SiJavascript className="text-[#F7DF1E]" />
-      )}
-
-      {technology.name === "TypeScript" && (
-        <SiTypescript className="text-[#3178C6]" />
-      )}
-
-      {technology.name === "Java" && <FaJava className="text-[#ED8B00]" />}
-
-      {technology.name === "Tailwind CSS" && (
-        <SiTailwindcss className="text-[#06B6D4]" />
-      )}
-
-      {technology.name === "Docker" && <SiDocker className="text-[#2496ED]" />}
-    </div>
+    <img
+      src={technology.icon}
+      alt={technology.name}
+      className=" mb-3 h-10 w-10 object-contain"
+    />
   );
 };
 
