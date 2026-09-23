9c827f59ce4228b713b9ce13099d07a8e62e5100
jahidrefat238-oss <jahidrefat238@gmail.com>
2026-09-23 12:24:48 +0600

    Fix technology icon rendering

--- src/components/TechnologySection/TechnologyCard/TechnologyCard.tsx
+++ src/components/TechnologySection/TechnologyCard/TechnologyCard.tsx
@@ -1,5 +1,18 @@
 import type { ITechnology } from "../../../types/technology";
-import { SiReact } from "react-icons/si";
+import { FaJava } from "react-icons/fa";
+import {
+  SiReact,
+  SiVuedotjs,
+  SiSvelte,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiPostgresql,
+  SiRedis,
+  SiJavascript,
+  SiTypescript,
+  SiTailwindcss,
+  SiDocker,
+} from "react-icons/si";
 interface TechnologyCardProps {
   technology: ITechnology;
 }
@@ -8,9 +21,50 @@ const TechnologyCard = ({ technology }: TechnologyCardProps) => {
   return (
     <div className="rounded-xl border border-gray-200 bg-white p-4 shadow-sm">
       <div className="text-3xl">
-        <SiReact />
-      </div>
+        {technology.name === "React" && <SiReact className="text-[#61DAFB]" />}
 
+        {technology.name === "Vue.js" && (
+          <SiVuedotjs className="text-[#4FC08D]" />
+        )}
+
+        {technology.name === "Svelte" && (
+          <SiSvelte className="text-[#FF3E00]" />
+        )}
+
+        {technology.name === "Next.js" && (
+          <SiNextdotjs className="text-black" />
+        )}
+
+        {technology.name === "Node.js" && (
+          <SiNodedotjs className="text-[#339933]" />
+        )}
+
+        {technology.name === "PostgreSQL" && (
+          <SiPostgresql className="text-[#4169E1]" />
+        )}
+
+        {technology.name === "Redis" && (
+          <img src="/redis_logo.png" alt="Redis" className="h-12 w-15" />
+        )}
+
+        {technology.name === "JavaScript" && (
+          <SiJavascript className="text-[#F7DF1E]" />
+        )}
+
+        {technology.name === "TypeScript" && (
+          <SiTypescript className="text-[#3178C6]" />
+        )}
+
+        {/* {technology.name === "Java" && <SiJava className="text-[#ED8B00]" />} */}
+        {technology.name === "Java" && <FaJava className="text-[#ED8B00]" />}
+        {technology.name === "Tailwind CSS" && (
+          <SiTailwindcss className="text-[#06B6D4]" />
+        )}
+
+        {technology.name === "Docker" && (
+          <SiDocker className="text-[#2496ED]" />
+        )}
+      </div>
       <div className="flex items-center justify-between">
         <h3 className="text-lg font-semibold">{technology.name}</h3>
 
